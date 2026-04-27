6c9b6428d58214d2aa84abefc305d283ac82f106
Ayoub Jabiri <ayoubjabiri.business@gmail.com>
2026-04-27 12:14:09 +0100

    Finished delete in array as state course

--- src/components/TheArray.jsx
+++ src/components/TheArray.jsx
@@ -4,16 +4,36 @@ export default function TheArray() {
     const [inputValue, setInputValue] = useState("");
 
     const [devices, setDevices] = useState([
-        "iphone",
-        "mac",
-        "samsung",
-        "windows",
+        { id: 1, title: "iphone" },
+        { id: 2, title: "mac" },
+        { id: 3, title: "samsung" },
+        { id: 4, title: "windows" },
     ]);
-    const devicesList = devices.map((device, i) => (
-        <i key={i} className="block">
-            {"=> " + device}
+    let newId = 5;
+
+    const devicesList = devices.map((device) => (
+        <i key={device.id} className="block">
+            {"=> " + device.title}{" "}
+            <button
+                className="bg-red-500 text-white text-xs rounded-md p-1 cursor-pointer"
+                onClick={() => {
+                    handleDelete(device.id);
+                }}
+            >
+                Delete
+            </button>
         </i>
     ));
+
+    function handleDelete(id) {
+        // const newDevices = [...devices];
+        // const index = newDevices.findIndex((el) => el.id == id);
+        // newDevices.splice(index, 1);
+
+        const newDevices = devices.filter((device) => device.id != id);
+        setDevices([...newDevices]);
+    }
+
     return (
         <div className="m-5">
             <ul>{devicesList}</ul>
@@ -33,7 +53,11 @@ export default function TheArray() {
                 <button
                     className="bg-green-500 rounded-md px-4 py-2 text-white block mt-2 cursor-pointer"
                     onClick={() => {
-                        setDevices([...devices, inputValue]);
+                        setDevices([
+                            ...devices,
+                            { id: newId, title: inputValue },
+                        ]);
+                        newId += 1;
                     }}
                 >
                     Add Device
